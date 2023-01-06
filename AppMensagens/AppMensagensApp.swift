018509df63d11ed308ae85a2f9a6a4efb3cf1e78
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-06 15:00:39 -0300

    Initial Commit

---
+++ AppMensagens/AppMensagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMensagensApp.swift
+//  AppMensagens
+//
+//  Created by Marcelo Hora on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AppMensagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
