d4d3b1b2747e7c9c11f39150772ec6c677e56199
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-07 01:43:07 -0300

    Update scroll

--- AppMensagens/Navigation/NavigationBar.swift
+++ AppMensagens/Navigation/NavigationBar.swift
@@ -9,38 +9,39 @@ import SwiftUI
 
 struct NavigationBar: View {
     var body: some View {
-        VStack{
-            Divider()
-            HStack{
-                Spacer()
-                Text("Featured ")
-                    .font(.title.bold())
-                    .foregroundColor(.primary)
+        ScrollView{
+            VStack{
+                Divider()
                 HStack{
-                    Spacer()
-                    Button(action: {}) {
-                        Image("Lupa")
-                            .resizable()
-                            .frame(width: 26, height: 26)
-                            .cornerRadius(10)
-                            .padding(8)
-                            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                    Text("Featured ")
+                        .font(.title.bold())
+                        .foregroundColor(.primary)
+                        .padding(.horizontal, 20)
+                    HStack{
+                        Spacer()
+                        Button(action: {}) {
+                            Image("Lupa")
+                                .resizable()
+                                .frame(width: 26, height: 26)
+                                .cornerRadius(10)
+                                .padding(8)
+                                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                            
+                        }
+                        Button(action: {}) {
+                            Image("Image 3")
+                                .resizable()
+                                .frame(width: 26, height: 26)
+                                .cornerRadius(10)
+                                .padding(8)
+                                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                            
+                        }
                         
                     }
-                    Button(action: {}) {
-                        Image("Image 3")
-                            .resizable()
-                            .frame(width: 26, height: 26)
-                            .cornerRadius(10)
-                            .padding(8)
-                            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
-                        
-                        
-                    }
-                    
                 }
+                Spacer()
             }
-            Spacer()
         }
     }
 }
